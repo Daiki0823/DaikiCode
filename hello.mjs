9ae00f47e32a22c8bad49bd9e3ea278772572393
daiki-ikarashi <igarashidaiki@MacBook-Pro.local>
2020-08-29 18:56:55 +0900

    pushできるかな

---
+++ hello.mjs
@@ -0,0 +1,13 @@
+< !DOCTYPE html >
+    <html lang="ja">
+        <head>
+            <meta charset="utf-8">
+                <title>Sample</title>
+        </head>
+            <body>
+                <script>
+                    console.log("Hello World!");
+                    console/log("Hi Navitime")
+                </script>
+            </body>
+</html>
